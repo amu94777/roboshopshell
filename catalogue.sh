0f98d617dd000882cd7ea61999fa2dde2be71b6a
amu94777 <amu94777@gmail.com>
2023-12-15 20:46:13 +0530

    shellscript for catalog

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+MONGODB-HOST=mongo.jaya123.shop
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -43,5 +44,32 @@ VALIDATE $? "APP DIRECTORY CREATION"
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
 
+cd /app 
 
+unzip /tmp/catalogue.zip 
+cd /app
+
+npm install &>> $LOGFILE
+VALIDATE $? "NPM INSTALLED"
+
+cp /home/centos/roboshopshell/catalog.service /etc/systemd/system/catalog.service
+VALIDATE $? "COPIED CATALOG.SERVICE"
+
+systemctl daemon-reload
+VALIDATE $? "DEAMON-RELOAD"
+
+systemctl enable catalogue
+VALIDATE $? "ENABLED CATALOG"
+
+systemctl start catalogue
+VALIDATE $? "STARTED CATALOG SERVICE"
+
+cp /home/centos/roboshopshell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "COPIED MONGO REPO"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "INSATLLED MONGO CLIENT"
+
+mongo --host $MONGODB-HOST </app/schema/catalogue.js
+VALIDATE $? "LOADING CATALOG DATA INTO MONGO DB"
 
