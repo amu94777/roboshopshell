872fe698ff2021bb04c739fead4667bee433888a
amu94777 <amu94777@gmail.com>
2023-12-15 17:23:40 +0530

    shellscript for mangodb

---
+++ mongodb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+VALIDATE(){
+ if [ $1 -ne 0 ] 
+ then
+    echo -e "$R ERROR ::::::$2 FAILED $N"
+    exit 1
+ else
+    echo -e "$G $2 is successfull $N"
+fi         
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:::you are not a root user $N"
+    exit 1
+else
+    echo -e "$G you are a root user $N
+fi
