5638a96b097f2a225af9dc76cd3dbb9d803f7017
amu94777 <amu94777@gmail.com>
2023-12-16 16:29:26 +0530

    user configuration using shellscript

---
+++ user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+VALIDATE(){
+ if [ $1 -ne 0 ] 
+ then
+    echo -e "$R ERROR ::::::$2 FAILED $N"
+    exit 1
+ else
+    echo -e "$G $2 is successfull $N"
+fi         
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:::you are not a root user $N"
+    exit 1
+else
+    echo -e "$G you are a root user $N"
+fi
+
+echo -e "$Y ****************NOW I AM CONFIGURAING USER CONFIGURATION**********$N"
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "DISABLED NODEJS"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "ENABLED NODEJS:18"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "INSATELLED NODEJS"
+
